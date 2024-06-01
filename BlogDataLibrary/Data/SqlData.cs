4044c25b57a243220a604a63a9fd19c39974aa59
Pirgor <Pirgor@PIRGOR-LAPTOP>
2024-06-01 14:24:14 +0800

    Add project files.

---
+++ BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,19 @@
+﻿using BlogDataLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogDataLibrary.Data
+{
+    public class SqlData
+    {
+        private ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDB";
+        public SqlData (ISqlDataAccess db)
+        {
+            _db = db; 
+        }
+    }
+}
